7efe08ad9aebb67a451a33af28a27d4a20160af0
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-06-23 14:02:17 +0900

    サービスクラスを追加

---
+++ src/main/java/com/example/service/TeamService.java
@@ -0,0 +1,26 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Team;
+import com.example.repository.TeamRepository;
+
+@Service
+@Transactional
+public class TeamService {
+
+	@Autowired
+	private TeamRepository repository;
+
+	public Team load(Integer id) {
+		return repository.load(id);
+	}
+
+	public List<Team> findAll() {
+		return repository.findAll();
+	}
+}
